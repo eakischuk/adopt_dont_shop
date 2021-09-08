8e0c67503930be33f97b7cbaa0883f15185f464c
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-08 08:29:03 -0600

    Add spec for admin applications show page with approval button

---
+++ spec/features/admin/applications/show_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin applications show page' do
+  before(:each) do
+    @shelter_1 = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    @shelter_2 = Shelter.create!(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
+    @shelter_3 = Shelter.create!(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+    @pet_1 = Pet.create!(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true, shelter: @shelter_1)
+    @pet_2 = Pet.create!(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true, shelter: @shelter_1)
+    @pet_3 = Pet.create!(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true, shelter: @shelter_2)
+    @shelter_1.pets << @pet_1
+    @shelter_1.pets << @pet_2
+    @shelter_3.pets << @pet_3
+    @app_1 = Application.create!(name: "Erika Kischuk", address: "455 Villa Ct. Morrison, CO 80228", description: "I love animals", status: "In Progress")
+    @app_2 = Application.create!(name: "Kate Hill", address: "67 S Montgomery St. Lakewood, CO 80227", description: "Dogs love me!", status: "In Progress")
+  end
+
+  it 'shows application attributes and pets' do
+    @app_1.pets << @pet_1
+    @app_1.pets << @pet_2
+
+    visit "/admin/applications/#{@app_1.id}"
+
+    expect(page).to have_content(@app_1.name)
+    expect(page).to have_content(@pet_2.name)
+    expect(page).to_not have_content(@pet_3.name)
+  end
+
+  it 'has approve button for each pet' do
+    @app_1.pets << @pet_1
+    @app_1.pets << @pet_2
+
+    visit "/admin/applications/#{@app_1.id}"
+
+    within("#pet-#{@pet_1.id}") do
+      expect(page).to have_button("Approve")
+    end
+
+    within("#pet-#{@pet_2.id}") do
+      expect(page).to have_button("Approve")
+      click_on "Approve"
+    end
+
+    expect(current_path).to eq("/admin/applications/#{@app_1.id}")
+
+    within("#pet-#{@pet_1.id}") do
+      expect(page).to have_button("Approve")
+    end
+    
+    within("#pet-#{@pet_2.id}") do
+      expect(page).to_not have_button("Approve")
+    end
+  end
+end
