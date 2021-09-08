f2bfcd3bbdb410d05ef33ebdf121580f9a5478af
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-08 09:29:26 -0600

    Allow update action to accept status params, add reject button and pass query strings on click

--- app/controllers/admin/application_pets_controller.rb
+++ app/controllers/admin/application_pets_controller.rb
@@ -2,7 +2,7 @@ class Admin::ApplicationPetsController < ApplicationController
   def update
     app_pet = Application.find(params[:app_id]).app_pet(params[:pet_id])
     app_pet.update({
-      status: "Approved"
+      status: params[:status]
       })
       app_pet.save
     redirect_to "/admin/applications/#{params[:app_id]}"
